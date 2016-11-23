39312d395ec6cb93fc230f9afffa410d007d2261
Andrea <alingenfelter2003@gmail.com>
2016-11-23 16:50:25 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+//const App = () => <h1>Hello GOL</h1>
+const App = require('./app')
+ReactDOM.render(<App />, document.getElementById('root'))
